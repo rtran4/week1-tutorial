8a3615b5808902acfd4008dc29c42ead28e4caed
Richard Tran <rtran4@mail.valenciacollege.edu>
2016-01-15 23:41:29 -0500

    step 4....

--- hello.py
+++ hello.py
@@ -1,6 +1,6 @@
 # replace the word 'hector' with your own name
 
 def hello_world():
-	return 'hello world, my name is ’
+	return 'hello world, my name is Richard Tran’
 
-print( hello_world() + ‘Richard Tran’ )
+print( hello_world() + ‘hector’ )
